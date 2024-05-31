f006dfdba01ade70ba669065546bdcbe412e3879
Andrew Teece <andrewteece@gmail.com>
2024-05-31 08:58:12 -0500

    js basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -22,9 +22,44 @@ console.log(typeof continent);
 console.log(typeof population);
 console.log(typeof isIsland);
 
-*/
+
 
 let age = 54;
 
 age = 55;
 console.log(age);
+
+*/
+
+// Math Operators
+
+const currentYear = 2024;
+const ageAndrew = currentYear - 1969;
+const ageAndrea = currentYear - 1972;
+console.log(ageAndrew, ageAndrea);
+
+console.log(ageAndrew * 2, ageAndrew / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Andrew';
+const lastName = 'Teece';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators
+
+let x = 10 + 5; //15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x); // 25
+
+// Comparison Operators
+
+console.log(ageAndrew > ageAndrea); // greater than
+console.log(ageAndrew < ageAndrea); // less than
+console.log(ageAndrea >= 18); // greater than or equal
+console.log(ageAndrew <= 18); // less than or equal
+
+console.log(currentYear - 1991 > currentYear - 2018);
