d15f59f949f939264d5da28ed8ea9c230f3322bd
Andrew Teece <andrewteece@gmail.com>
2024-05-31 07:44:14 -0500

    js data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,21 @@
+/*
 let js = 'Amazing';
 
 let firstName = 'Andrew';
 console.log(firstName);
+*/
+let country = 'United States';
+let continent = 'North America';
+let population = 230;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language = '';
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
