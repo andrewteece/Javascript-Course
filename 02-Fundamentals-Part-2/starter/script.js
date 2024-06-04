ae210cb141096eb2e950b20df516732fdc972bdf
Andrew Teece <andrewteece@gmail.com>
2024-06-04 09:42:12 -0500

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,4 +8,20 @@ if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive!');
 */
 
-function logger() {}
+function logger() {
+  console.log('My name is Andrew');
+}
+
+// calling / running / invoking - the function
+logger();
+
+function foodProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const fruitJuice = foodProcessor(5, 2);
+console.log(fruitJuice);
+
+const appleOrangeJuice = foodProcessor(5, 9);
+console.log(appleOrangeJuice);
