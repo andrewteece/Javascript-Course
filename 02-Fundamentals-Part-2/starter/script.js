214d452838b954ea6d7720dc3f3c2e810fe78ff2
Andrew Teece <andrewteece@gmail.com>
2024-06-06 17:51:38 -0500

    function declarations/expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,7 +6,7 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive!');
-*/
+
 
 function logger() {
   console.log('My name is Andrew');
@@ -25,3 +25,22 @@ console.log(fruitJuice);
 
 const appleOrangeJuice = foodProcessor(5, 9);
 console.log(appleOrangeJuice);
+
+*/
+
+// Function Declaration (has a function Name)
+
+const age1 = calcAge1(1969);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+// Function Expression (function name is omitted)
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1969);
+console.log(age1, age2);
