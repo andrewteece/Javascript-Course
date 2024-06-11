b182aa0db2de8ed25050bf29c4683d8e76594c49
Andrew Teece <andrewteece@gmail.com>
2024-06-11 09:46:06 -0500

    array challenge calc tips

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -266,7 +266,7 @@ console.log(age1, age2, age3);
 const ages = [calcAge(years[0]), calcAge(years[2]), calcAge(years[4])];
 console.log(ages);
 
-*/
+
 
 // Array Methods
 
@@ -295,3 +295,16 @@ console.log(friends.includes('Bob'));
 if (friends.includes('William')) {
   console.log('You have a friend called William');
 }
+
+*/
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
