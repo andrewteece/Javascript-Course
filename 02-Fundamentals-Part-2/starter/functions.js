12570fda4debda3b280681f871fa464deba80fa4
Andrew Teece <andrewteece@gmail.com>
2024-06-15 15:38:32 -0500

    build and call functions

--- 02-Fundamentals-Part-2/starter/functions.js
+++ 02-Fundamentals-Part-2/starter/functions.js
@@ -1,4 +1,18 @@
 // Arrays
 
-const train1 = ['wheat', 'barley', 'salt', 'sugar', 'potato'];
-const train2 = ['coal', 'ore'];
+//const train1 = ['wheat', 'barley', 'salt', 'sugar', 'potato'];
+//const train2 = ['coal', 'ore'];
+
+// Building and calling functions
+
+/*
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+
+function listArrayItems(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(i, arr[i]);
+  }
+}
+listArrayItems(colors);
+
+*/
