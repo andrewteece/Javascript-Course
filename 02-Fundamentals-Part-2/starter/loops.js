046c17615b06d9abe71eebb240f42a39e6c712ed
Andrew Teece <andrewteece@gmail.com>
2024-06-15 18:07:19 -0500

    while do loops

--- 02-Fundamentals-Part-2/starter/loops.js
+++ 02-Fundamentals-Part-2/starter/loops.js
@@ -141,4 +141,50 @@ for (let i = 0; i <= 20; i++) {
   console.log(i);
 }
 
-*/
+
+
+// while loop
+
+let i = 0;
+
+while (i <= 20) {
+  console.log('Number ' + i);
+  i++;
+}
+
+
+
+// loop over arrays
+
+let i = 0;
+const arr = [10, 20, 30, 40];
+
+while (i < arr.length) {
+  console.log(arr[i]);
+  i++;
+}
+
+
+// nesting while loops
+
+let i = 1;
+
+while (i <= 5) {
+  console.log('Number ' + i);
+
+  let j = 1;
+  while (j <= 5) {
+    console.log(`${i} * ${j} = ${i * j}`);
+    j++;
+  }
+  i++;
+}
+    */
+
+// Do While loops
+
+let i = 21;
+do {
+  console.log('Number ' + i);
+  i++;
+} while (i <= 20);
