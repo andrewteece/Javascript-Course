d48cbc36150318b278968cfbec46fa30e5e911bd
Andrew Teece <andrewteece@gmail.com>
2024-06-15 16:16:22 -0500

    break and continue

--- 02-Fundamentals-Part-2/starter/loops.js
+++ 02-Fundamentals-Part-2/starter/loops.js
@@ -104,7 +104,7 @@ for (let i = 1; i <= 5; i++) {
   }
 }
 
- */
+
 
 // loop through an array
 
@@ -117,3 +117,28 @@ for (let i = 0; i < names.length; i++) {
     console.log(names[i]);
   }
 }
+
+
+
+// Break
+
+for (let i = 0; i <= 20; i++) {
+  if (i === 15) {
+    console.log('Breaking...');
+    break;
+  }
+  console.log(i);
+}
+   
+
+// Continue
+
+for (let i = 0; i <= 20; i++) {
+  if (i === 13) {
+    console.log('Skipping 13...');
+    continue;
+  }
+  console.log(i);
+}
+
+*/
