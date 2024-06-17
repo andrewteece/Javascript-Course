d33fa80b1905ff92722d91937c160961eaaea400
Andrew Teece <andrewteece@gmail.com>
2024-06-17 09:51:27 -0500

    object literals dot and bracket

---
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -0,0 +1,44 @@
+// Object Literals and Dot Notation
+
+const assistantMgr = {
+  rangeTilesPerTurn: 3,
+  socialSkills: 30,
+  streetSmarts: 30,
+  health: 40,
+  specialAbility: 'young and ambitious',
+  greeting: "Let's make some money",
+};
+
+assistantMgr.yearsOnJob = 5;
+
+console.log(assistantMgr);
+
+// Object Literals with Bracket Notation
+
+let house2 = {};
+house2['rooms'] = 4;
+house2['color'] = 'red';
+house2['priceUSD'] = 12345;
+console.log(house2);
+
+let car = {};
+car.color = 'red';
+car['color'] = 'green';
+car['speed'] = 200;
+car.speed = 100;
+
+car['number of doors'] = 4;
+
+console.log(car);
+
+// Finally, there's one really useful thing that bracket notation has but is not available in the dot notation: It can evaluate expressions.
+
+let arrOfKeys = ['speed', 'altitude', 'color'];
+let drone = {
+  speed: 100,
+  altitude: 200,
+  color: 'red',
+};
+for (let i = 0; i < arrOfKeys.length; i++) {
+  console.log(drone[arrOfKeys[i]]);
+}
