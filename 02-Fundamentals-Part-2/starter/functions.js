13f6985bb1560c5831002ad635057231975c5c38
Andrew Teece <andrewteece@gmail.com>
2024-06-17 17:21:34 -0500

    array and object challenge

--- 02-Fundamentals-Part-2/starter/functions.js
+++ 02-Fundamentals-Part-2/starter/functions.js
@@ -80,3 +80,42 @@ for (let i = 0; i < greeting.length; i++) {
 }
 
 */
+// Exercise: Creating arrays and objects
+/*
+Tasks to complete
+Create a new empty array literal and assign it to the variable clothes.
+
+Add 5 of your favorite items of clothing as strings using the push() method.
+
+Remove the fifth piece of clothing from the array using the pop() method.
+
+Add a new piece of clothing using the push() method.
+
+Use console.log to show the third item from the clothes array in the console.
+
+Create a new empty object literal and assign it to the variable favCar.
+
+Using the dot notation, assign a color property to the favCar object and give it a string value with the color of your choice.
+
+Using the dot notation, assign a covertible property to the favCar object and give it a boolean value of your choice.
+
+Use the console to log the entire favCar object.
+*/
+
+const clothes = [];
+clothes.push('tank-top');
+clothes.push('swimsuit');
+clothes.push('raincoat');
+clothes.push('beach pants');
+clothes.push('beach shirt');
+
+clothes.pop();
+clothes.push('dress shirt');
+console.log(clothes);
+console.log(clothes[2]);
+
+const favCar = {};
+favCar.color = 'red';
+favCar.convertible = true;
+
+console.log(favCar);
