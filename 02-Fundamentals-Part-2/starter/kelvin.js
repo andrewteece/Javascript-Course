ca5e8ab24822135ecf55c5aa4ee0f6ce61fa1548
Andrew Teece <andrewteece@gmail.com>
2024-06-19 08:37:41 -0500

    dog years conversion

--- 02-Fundamentals-Part-2/starter/kelvin.js
+++ 02-Fundamentals-Part-2/starter/kelvin.js
@@ -1,8 +1,36 @@
+/*
 // temperature in kelvin
-const kelvin = 500;
+const kelvin = 293;
 // kelvin minus celsius
 const celsius = kelvin - 273;
 // equation to calculate fahrenheit
 const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
 console.log(`The temperature is ${fahrenheit}
 	degrees fahrenheit.`);
+
+
+
+// celsius to newton
+
+const celsius = 28;
+const newton = Math.floor(celsius * (33 / 100));
+console.log(`The temperature is ${newton} newton`);
+
+*/
+
+// Dog Years
+
+let myAge = 55;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+laterYears = myAge - 2;
+laterYears *= 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+let myName = 'Andrew'.toLowerCase();
+
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
