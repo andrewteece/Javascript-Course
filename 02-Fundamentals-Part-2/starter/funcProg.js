bb4f673b2f4852782a40b186abc30faaf8e3bb29
Andrew Teece <andrewteece@gmail.com>
2024-06-20 08:14:05 -0500

    first-class functions

--- 02-Fundamentals-Part-2/starter/funcProg.js
+++ 02-Fundamentals-Part-2/starter/funcProg.js
@@ -1,5 +1,6 @@
 // functional programming
 
+/*
 var currencyOne = 100;
 var currencyTwo = 0;
 var exchangeRate = 1.2;
@@ -11,3 +12,29 @@ function convertCurrency(amount, rate) {
 currencyTwo = convertCurrency(currencyOne, exchangeRate);
 
 console.log(currencyTwo);
+*/
+
+// First-class Functions
+
+function addTwoNums(a, b) {
+  console.log(a + b);
+}
+
+function randomNum() {
+  return Math.floor(Math.random() * 10 + 1);
+}
+
+function specificNum() {
+  return 42;
+}
+
+let useRandom = true;
+
+let getNumber;
+if (useRandom) {
+  getNumber = randomNum;
+} else {
+  getNumber = specificNum;
+}
+
+addTwoNums(getNumber(), getNumber());
