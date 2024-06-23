00cc542c691414fc26c0ad15fd8443f18eb48faf
Andrew Teece <andrewteece@gmail.com>
2024-06-23 08:14:22 -0500

    OOP challenge

--- 02-Fundamentals-Part-2/starter/oop.js
+++ 02-Fundamentals-Part-2/starter/oop.js
@@ -117,7 +117,7 @@ console.log('penguin1: ', penguin1);
 console.log('penguin1 has feathers:', penguin1.hasFeathers);
 console.log('penguin1 can fly:', penguin1.canFly);
 
-*/
+
 
 // Designing an OO Program
 
@@ -260,3 +260,62 @@ leo.makeSound(true); // purr, meow
 var cuddles = new Tiger();
 cuddles.makeSound(false); // Roar!
 cuddles.makeSound(true); // purr, Roar!
+
+*/
+
+// OOP Challenge
+
+// Task 1: Code a Person Class
+class Person {
+  constructor(name = 'Tom', age = 20, energy = 100) {
+    this.name = name;
+    this.age = age;
+    this.energy = energy;
+  }
+  sleep() {
+    this.energy += 10;
+  }
+  doSomethingFun() {
+    this.energy -= 10;
+  }
+}
+
+// Task #2 : Code a Worker Class
+class Worker extends Person {
+  constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+    super(name, age, energy);
+    this.xp = xp;
+    this.hourlyWage = hourlyWage;
+  }
+  goToWork() {
+    this.xp += 10;
+  }
+}
+
+// Task 3: Code an Intern object, run methods
+
+function intern() {
+  let intern = new Worker(
+    (name = 'Bob'),
+    (age = 21),
+    (energy = 110),
+    (xp = 0),
+    (hourlyWage = 10)
+  );
+  intern.goToWork();
+  return intern;
+}
+
+// Task 4: Code a Manager object, run methods
+
+function manager() {
+  let manager = new Worker(
+    (name = 'Alice'),
+    (age = 30),
+    (energy = 120),
+    (xp = 100),
+    (hourlyWage = 30)
+  );
+  manager.doSomethingFun();
+  return manager;
+}
