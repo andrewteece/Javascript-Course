bb46a0c9b00e25d63ab7c53cb5592c1619007c4d
Andrew Teece <andrewteece@gmail.com>
2024-06-24 08:49:07 -0500

    continue and break

--- 02-Fundamentals-Part-2/starter/loops.js
+++ 02-Fundamentals-Part-2/starter/loops.js
@@ -221,6 +221,38 @@ const person = [
   true,
 ];
 
+const types = [];
+
 for (let i = 0; i < person.length; i++) {
+  // Reading from person array
+  console.log(person[i], typeof person[i]);
+
+  // Filling types array
+  //types[i] = typeof person[i];
+
+  types.push(typeof person[i]);
+}
+
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2024 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+
+console.log('--- ONLY STRINGS ---');
+for (let i = 0; i < person.length; i++) {
+  if (typeof person[i] !== 'string') continue;
+  console.log(person[i], typeof person[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---');
+for (let i = 0; i < person.length; i++) {
+  if (typeof person[i] === 'number') break;
   console.log(person[i], typeof person[i]);
 }
