48963dd03f405dbfe76473adb5c30ca068c58146
Andrew Teece <andrewteece@gmail.com>
2024-06-25 18:09:26 -0500

    map and set in js

--- 02-Fundamentals-Part-2/starter/objects.js
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -141,7 +141,7 @@ for (prop of Object.keys(sportsCar)) {
 console.log(`Iterating over oject's OWN PROPERTIES only1`);
 console.log(sportsCar);
 
-*/
+
 
 // Programming Assignment: Array and Object Iteration
 
@@ -193,3 +193,59 @@ function animalCan() {
 }
 
 animalCan();
+
+
+
+// Data Structures
+
+// arrays
+
+let grades = [75, 95, 90, 85, 80, 85];
+
+let gradesSum = 0;
+
+for (i = 0; i < grades.length; i++) {
+  gradesSum += grades[i];
+}
+console.log(gradesSum / grades.length);
+
+// Working with Arrays
+// forEach() method
+
+const veggies = ['onion', 'garlic', 'potato'];
+veggies.forEach(function (veggie, index) {
+  console.log(`${index}. ${veggie}`);
+});
+
+// filter() method
+
+const nums = [0, 10, 20, 30, 40, 50];
+nums.filter(function (num) {
+  console.log(num);
+  return num > 20;
+});
+
+*/
+
+// map method
+
+[0, 10, 20, 30, 40, 50].map(function (num) {
+  console.log(num);
+  return num / 10;
+});
+
+// working with Maps in JS
+
+let bestBoxers = new Map();
+bestBoxers.set(1, 'The Champion');
+bestBoxers.set(2, 'The Runner-up');
+bestBoxers.set(3, 'The third place');
+
+console.log(bestBoxers);
+bestBoxers.get(1);
+
+// working with Sets in JS
+
+const repetitiveFruits = ['apple', 'pear', 'apple', 'pear', 'plum', 'apple'];
+const uniqueFruits = new Set(repetitiveFruits);
+console.log(uniqueFruits);
