91d5e2d0f574d5777f357abcf8b79635a335432b
Andrew Teece <andrewteece@gmail.com>
2024-06-24 19:17:37 -0500

    challenge #4 with loop

--- 02-Fundamentals-Part-2/starter/loops.js
+++ 02-Fundamentals-Part-2/starter/loops.js
@@ -324,3 +324,26 @@ const calcTip = function (bill) {
 };
 
 let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills, tips, totals);
+
+let calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    //sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 4, 9]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+console.log(calcAverage(bills));
