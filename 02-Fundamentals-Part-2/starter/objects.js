da2488306742033e6a716cbb3020a223674b2c21
Andrew Teece <andrewteece@gmail.com>
2024-06-25 09:02:10 -0500

    for/in loops and objects

--- 02-Fundamentals-Part-2/starter/objects.js
+++ 02-Fundamentals-Part-2/starter/objects.js
@@ -84,3 +84,59 @@ car.lightsOn();
 */
 
 // De-structuring array and objects
+
+const clothingItem = {
+  price: 50,
+  color: 'beige',
+  material: 'cotton',
+  season: 'autumn',
+};
+
+for (const key of Object.keys(clothingItem)) {
+  console.log(key, ':', clothingItem[key]);
+}
+
+function textBracketsDynamicAccess() {
+  let dynamicKey;
+  if (Math.random() > 0.5) {
+    dynamicKey = 'speed';
+  } else {
+    dynamicKey = 'color';
+  }
+
+  const drone = {
+    speed: 15,
+    color: 'orange',
+  };
+  console.log(drone[dynamicKey]);
+}
+
+textBracketsDynamicAccess();
+
+// For of loops and objects
+
+// create object
+const car = {
+  engine: true,
+  steering: true,
+  speed: 'slow',
+};
+
+const sportsCar = Object.create(car);
+sportsCar.speed = 'fast';
+console.log(`The sportsCar object: `, sportsCar);
+
+console.log(`--- for-in is unreliable ---`);
+for (prop in sportsCar) {
+  console.log(prop);
+}
+
+console.log(`Iterating over object AND its prototype`);
+
+console.log(`--- for-of is reliable ----`);
+for (prop of Object.keys(sportsCar)) {
+  console.log(prop + ': ' + sportsCar[prop]);
+}
+
+console.log(`Iterating over oject's OWN PROPERTIES only1`);
+console.log(sportsCar);
